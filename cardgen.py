831c1a6588a7f39fba6614cc57425cf8658d315e
Alvaro Gudiswitz <alvarogudis@gmail.com>
2023-07-23 11:00:49 -0500

    Moved files to the resources folder. Code for onions and RPS. Onions may need a do-over.

--- cardgen.py
+++ cardgen.py
@@ -2,6 +2,14 @@ from PIL import Image, ImageDraw, ImageFont
 import csv
 import shutil
 import os
+from enum import Enum
+
+class ResourceType (Enum):
+    NUMBERS = 1
+    RPS = 2
+    ONION = 3
+
+currentResource = ResourceType.ONION 
 
 isStarEnabled = True
 
@@ -24,6 +32,19 @@ def get_wrapped_text(text: str, font: ImageFont.ImageFont,
                 lines.append(word)
         return '\n'.join(lines)
 
+def GetRPS(costString):
+    cost = int(costString)
+    if cost == 1:
+        image = Image.open('resources/rock.png')
+    elif cost == 2:
+        image = Image.open('resources/paper.png')
+    elif cost == 3:
+        image = Image.open('resources/scissors.png')
+    else:
+        return None
+    return image
+
+    
 def draw_card( name, cost, value, effect, rotation):
     for direction in "LR":
         finalImage = Image.new("RGB", (600, 825), color=0)
@@ -43,13 +64,24 @@ def draw_card( name, cost, value, effect, rotation):
         draw.text((250, 120), name, font=font)
 
         # cost text
-        font = ImageFont.truetype("fonts/Roboto-Bold.ttf", size=110)
-        draw.text((100, 100), str(cost), font=font, fill=0)
+        if currentResource == ResourceType.NUMBERS:
+            font = ImageFont.truetype("fonts/Roboto-Bold.ttf", size=110)
+            draw.text((100, 100), str(cost), font=font, fill=0)
+        elif currentResource == ResourceType.RPS:
+            image = GetRPS(cost)
+            image = image.resize((160, 160))
+            finalImage.paste(image, (60, 90), mask=image)
+        elif currentResource == ResourceType.ONION:
+            image = Image.open('resources/Onion' + cost + ".png")
+            image = image.resize((160, 160))
+            finalImage.paste(image, (60, 90), mask=image)
+
+
         if isStarEnabled:
             draw.text((130, 680), str(value), font=font, fill=0, anchor="mm")
         
+        # Effect descriptions
         font = ImageFont.truetype("fonts/Roboto-Bold.ttf", size=50)
-        
         effectText = get_wrapped_text(effect, font, 350)
         draw.multiline_text((210,220),effectText, fill=0, font=font)
 
@@ -75,6 +107,11 @@ finally:
 
 with open('cards.csv', newline='') as csvfile:
     for row in csv.reader(csvfile, delimiter=';', quotechar='|'):
-        draw_card(row[0], row[1], row[2], row[4], row[3])
+        if currentResource == ResourceType.RPS:
+            draw_card(row[0] + 'R', 1, row[2], row[4], row[3])
+            draw_card(row[0] + "P", 2, row[2], row[4], row[3])
+            draw_card(row[0] + "S", 3, row[2], row[4], row[3])
+        else:
+            draw_card(row[0], row[1], row[2], row[4], row[3])
 
 #draw_card("Lockdown", 6, 6, "Once the second lockdown is played, every other player gets one more turn.", 1)
